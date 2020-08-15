8502d9709cd3a859cfd80065874fe2897e506fcf
faran4engg <faran4engg@gmail.com>
2020-08-15 12:41:12 +0200

    order by jugaad

--- src/Pages/Posts/Posts.jsx
+++ src/Pages/Posts/Posts.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Post from "../../Components/Post/Post";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import Post from '../../Components/Post/Post';
+import axios from 'axios';
 const Posts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getPost = async () => {
       const { data } = await axios.get(
-        "https://fayez-instagram.firebaseio.com/posts.json"
+        `https://fayez-instagram.firebaseio.com/posts.json?orderBy="timestamp"`
       );
 
       if (data) {
@@ -25,8 +25,8 @@ const Posts = () => {
   }, []);
 
   return (
-    <div className="content-wrapper">
-      <div className="container d-flex align-items-center justify-content-center flex-column">
+    <div className='content-wrapper'>
+      <div className='container d-flex align-items-center justify-content-center flex-column'>
         {posts.map((post) => {
           return <Post key={post.timestamp} {...post} />;
         })}
